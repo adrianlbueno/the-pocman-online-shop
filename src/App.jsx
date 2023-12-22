735de92ccb83718a72c57d13cfe5f8464785eb34
Adrian <adrianbueno095@gmail.com>
2023-12-22 15:01:20 +0100

    tailwindcss setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className=" text-blue-400 text-3xl font-bold underline">
       Hello world!
     </h1>
   )
