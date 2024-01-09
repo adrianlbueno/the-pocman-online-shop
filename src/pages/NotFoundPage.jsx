b566ca5d791b56fb3a627e5be943bdaec1d5ec52
Adrian <adrianbueno095@gmail.com>
2024-01-09 12:44:42 +0100

    add styling to the not found page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,8 +1,11 @@
 const NotFound = () => {
     return (
         <>
-            <h1>404 - Page Not Found</h1>
-            <p>Sorry, the page you are looking for could not be found.</p>
+            <div className="bg-gray-200 w-full px-16 md:px-0 h-screen flex items-center justify-center">
+
+                <h1>404 - Page Not Found</h1>
+                <p>Sorry, the page you are looking for could not be found.</p>
+            </div>
         </>
     );
 }
