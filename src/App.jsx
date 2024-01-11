7e6977ac1caf8bf8a236d311b69beec9a0dce9e9
Adrian <adrianbueno095@gmail.com>
2024-01-11 01:20:40 +0100

    refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { Route, Routes } from "react-router-dom";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AboutPage from "./pages/AboutPage";
 import Contact from "./pages/Contact";
 import HomePage from "./pages/HomePage";
@@ -11,18 +10,18 @@ import SignUpPage from "./pages/SignUpPage";
 const App = () => {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="login" element={<LogInPage />} />
+          <Route path="signUp" element={<SignUpPage />} />
+          <Route path="illustrations/:illustrationId" element={<ItemDetailsPage />} />
 
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="about" element={<AboutPage />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="login" element={<LogInPage />} />
-        <Route path="signUp" element={<SignUpPage />} />
-        <Route path="illustrations/:illustrationId" element={<ItemDetailsPage />} />
-
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
 
     </>
   )
