a0c585296612a768411cbae75d5713a570e5f9d9
Adrian <adrianbueno095@gmail.com>
2024-01-11 15:28:11 +0100

    fix reack lionk onCLick prevent

--- src/components/EcommerCard.jsx
+++ src/components/EcommerCard.jsx
@@ -28,7 +28,10 @@ const EcommerceCard = ({ id, title, description, price, url }) => {
                     <button
                         className="block w-full select-none rounded-lg bg-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                         type="button"
-                        onClick={() => addToCart(id)}
+                        onClick={(event) => {
+                            event.preventDefault()
+                            addToCart(id)
+                        }}
                     >
                         Add to Cart
                     </button>
