21aae9ff0f3fb514e71294fa92b8a74fa25eba7e
Adrian <adrianbueno095@gmail.com>
2024-01-12 02:18:03 +0100

    add title and quantity

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -14,14 +14,14 @@ const CartItem = (props) => {
                 <div>
                     <div className="flex justify-between text-base font-medium text-gray-900">
                         <h3>
-                            {/* <a href={illustration.href}>{illustration.name}</a> */}
+                            {title}
                         </h3>
                         <p className="ml-4">{price}</p>
                     </div>
                     {/* <p className="mt-1 text-sm text-gray-500">{illustration.color}</p> */}
                 </div>
                 <div className="flex flex-1 items-end justify-between text-sm">
-                    {/* <p className="text-gray-500">Qty {illustration.quantity}</p> */}
+                    <p className="text-gray-500">Qty 1</p>
 
                     <div className="flex">
                         <button
@@ -33,7 +33,7 @@ const CartItem = (props) => {
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
 
     );
 }
