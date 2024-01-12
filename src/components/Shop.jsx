a69243094e8b5b0c60dc3e7386de124fae0591a5
Adrian <adrianbueno095@gmail.com>
2024-01-12 02:29:11 +0100

    add justify center

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -9,24 +9,22 @@ const Shop = () => {
 
     return (
         <>
-            <section className="text-gray-600 body-font">
-                <div className="container px-5 py-24 mx-auto">
-                    <div className="flex flex-col text-center w-full mb-20">
-                        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">The Pocman Illustration</h1>
-                        <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Are you who you want to be?</p>
-                    </div>
-                    <div className="flex flex-wrap -m-4">
-                        {illustrations && illustrations.map((illustration) => (
-                            <div key={illustration.id}>
-                                <Link to={`/illustrations/${illustration.id}`}>
-                                    <EcommerceCard id={illustration.id} title={illustration.title} description={illustration.description} price={illustration.price} url={illustration.image} />
-                                </Link>
-                            </div>
-                        ))}
-                    </div>
-
+            <div className="container px-5 py-24 mx-auto">
+                <div className="flex flex-col text-center w-full mb-20">
+                    <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">The Pocman Illustration</h1>
+                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Are you who you want to be?</p>
                 </div>
-            </section >
+                <div className="flex flex-wrap -m-4 justify-center" >
+                    {illustrations && illustrations.map((illustration) => (
+                        <div key={illustration.id}>
+                            <Link to={`/illustrations/${illustration.id}`}>
+                                <EcommerceCard id={illustration.id} title={illustration.title} description={illustration.description} price={illustration.price} url={illustration.image} />
+                            </Link>
+                        </div>
+                    ))}
+                </div>
+
+            </div>
         </>);
 
 }
