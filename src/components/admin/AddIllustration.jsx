9d3f54abf3c6a1be853ed38fe306e73cdcc09ee7
Adrian <adrianbueno095@gmail.com>
2024-01-14 12:04:06 +0100

    refactor add illustration component

--- src/components/admin/AddIllustration.jsx
+++ src/components/admin/AddIllustration.jsx
@@ -8,28 +8,33 @@ export const AddIllustration = () => {
     const { addIllustration, illustrations } = useContext(GlobalContext);
     console.log('illustrations', illustrations);
 
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
-    const [price, setPrice] = useState("");
-    const [image, setImage] = useState("");
+    const [form, setForm] = useState({
+        title: "",
+        description: "",
+        price: "",
+        image: ""
+    });
 
-    const onSubmit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         const newIllustration = {
-            id: illustrations.length + 1,
-            title,
-            description,
-            price: parseFloat(price),
-            image,
+            form
         };
         addIllustration(newIllustration);
         history.push("/");
     };
 
+    const handleChange = (e) => {
+        setForm(prev => ({
+            ...prev,
+            [e.target.name]: e.target.value, e
+        }))
+    }
+
     return (
         <>
             <div className="w-full max-w-sm container mt-20 mx-auto">
-                <form onSubmit={onSubmit}>
+                <form onSubmit={handleSubmit}>
                     <div className="w-full mb-5">
                         <label
                             className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
@@ -39,8 +44,8 @@ export const AddIllustration = () => {
                         </label>
                         <input
                             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:text-gray-600"
-                            value={title}
-                            onChange={(e) => setTitle(e.target.value)}
+                            value={form.title}
+                            onChange={handleChange}
                             type="text"
                             placeholder="Enter title"
                         />
@@ -53,9 +58,9 @@ export const AddIllustration = () => {
                             Description
                         </label>
                         <input
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:text-gray-600 focus:shadow-outline"
-                            value={description}
-                            onChange={(e) => setDescription(e.target.value)}
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gform.ray-700 leading-tight focus:text-gray-600 focus:shadow-outline"
+                            value={form.description}
+                            onChange={handleChange}
                             type="text"
                             placeholder="Enter description"
                         />
@@ -69,8 +74,8 @@ export const AddIllustration = () => {
                         </label>
                         <input
                             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:text-gray-600"
-                            value={price}
-                            onChange={(e) => setPrice(e.target.value)}
+                            value={form.price}
+                            onChange={handleChange}
                             type="text"
                             placeholder="Enter price"
                         />
@@ -84,8 +89,8 @@ export const AddIllustration = () => {
                         </label>
                         <input
                             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:text-gray-600 focus:shadow-outline"
-                            value={image}
-                            onChange={(e) => setImage(e.target.value)}
+                            value={form.image}
+                            onChange={handleChange}
                             type="text"
                             placeholder="Enter image URL"
                         />
