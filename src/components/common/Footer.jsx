835177ac96f8b939b4a48cbc8a3a2d15776167ba
Adrian <adrianbueno095@gmail.com>
2024-06-11 22:59:55 +0200

    remove whitespaces

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -5,12 +5,13 @@ const getYear = () => {
 const Footer = () => {
     return (
         <>
-            <footer >
+            <footer>
                 <div className="w-full max-w-full sm:w-3/4 mx-auto text-center">
                     <span className="text-sm text-gray-600 py-1"> &copy;{getYear()} All Rights Reversed by Adrian Bueno</span>
                 </div>
             </footer>
-        </>);
+        </>
+    );
 }
 
 export default Footer;
