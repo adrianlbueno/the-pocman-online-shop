40dbdeeedd341f6472484db7c12b553ae41544f0
adrianbuenoo095 <adrianbueno095@gmail.com>
2024-06-20 08:38:21 +0200

    testing something

--- src/utils/MultiContext.js
+++ src/utils/MultiContext.js
@@ -1,22 +1,11 @@
 import { createContext, memo, useContext } from "react";
-
 export const MultiContext = createContext(null);
 MultiContext.displayName = "MultiContext";
 
 export const MultiContextProvider = memo(
-    function({ map, children }) {
-        const contextMap = {};
-        for (const i in map) {
-            contextMap[i] = useContext(map[i]);
-        }
+    function(){
 
-        return (
-            <MultiContext.Provider value={contextMap}>
-                {children}
-            </MultiContext.Provider>
-        );
-    },
-    (prevProps, nextProps) => isEqual(prevProps.children, nextProps.children)
+    }
 );
 
 MultiContextProvider.displayName = "MultiContextProvider";
