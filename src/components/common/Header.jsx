92e0fcb30586431a5867efd2eb0581ec4605a4d6
adrianbuenoo095 <adrianbueno095@gmail.com>
2024-11-25 23:34:24 +0100

    add a picture element

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,20 +1,24 @@
 import headerImage from "../../assets/bg-illustration-pocmam.jpg";
+import pocmanImage from "../../assets/bg-illustration-pocmam_mobile.jpg"
 
 const Header = () => {
     return (
         <>
             <header>
-                <div id="Home"
-                     className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover"
-                     style={{backgroundImage: `url(${headerImage})`}}
-                >
-                    <div className="text-white">
+                <div id="Home">
+
+                    <picture>
+                        <source srcSet={headerImage} media="(min-width: 1240px)"/>
+                        <img src={pocmanImage} alt="image of girl seated on bed"/>
+                    </picture>
+                    <div className="absolute self-center">
                         <h1 className="text-4xl font-bold ">
                             Welcome
                         </h1>
                     </div>
 
                 </div>
+
             </header>
         </>
     );
