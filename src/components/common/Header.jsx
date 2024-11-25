11290d6397373699cbe942446208c511a5465581
adrianbuenoo095 <adrianbueno095@gmail.com>
2024-11-25 23:50:28 +0100

    center text

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -6,19 +6,20 @@ const Header = () => {
         <>
             <header>
                 <div id="Home">
-
+                    <div className="w-full h-screen'">
                     <picture>
                         <source srcSet={headerImage} media="(min-width: 1240px)"/>
                         <img src={pocmanImage} alt="image of girl seated on bed"/>
                     </picture>
-                    <div className="absolute self-center">
-                        <h1 className="text-4xl font-bold ">
-                            Welcome
-                        </h1>
+                        <div
+                            className="absolute text-2xl md:text-7xl text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                            <h1 className="text-4xl font-bold">
+                                Welcome
+                            </h1>
+                        </div>
+
                     </div>
-
                 </div>
-
             </header>
         </>
     );
