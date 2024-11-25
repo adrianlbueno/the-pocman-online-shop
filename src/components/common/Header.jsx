13ede9aa385128bbd9a829ee2329cab030e07c5b
adrianbuenoo095 <adrianbueno095@gmail.com>
2024-11-26 00:02:06 +0100

    add an overlay to picture

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -6,10 +6,12 @@ const Header = () => {
         <>
             <header>
                 <div id="Home" className="flex items-center">
+                    <div className="relative w-max before:absolute before:left-0 before:right-0 before:top-0 before:z-10 before:h-full before:w-full before:bg-gray-800 before:opacity-30">
                     <picture>
                         <source srcSet={headerImage} media="(min-width: 1240px)"/>
                         <img src={pocmanImage} alt="image of girl seated on bed"/>
                     </picture>
+                    </div>
                         <div
                             className="absolute text-2xl md:text-7xl text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                             <h1 className="text-4xl font-bold">
