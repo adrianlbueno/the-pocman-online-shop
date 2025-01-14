c53db4a9c0afa8e9ce121e24f50c2e334e91190a
adrianbuenoo095 <adrianbueno095@gmail.com>
2025-01-14 14:11:10 +0100

    install Sentry plugin

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -12,13 +12,13 @@ const Header = () => {
                         <img src={pocmanImage} alt="image of girl seated on bed"/>
                     </picture>
                     </div>
-                        <div
-                            className="absolute text-2xl md:text-7xl text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-                            <h1 className="text-4xl font-bold">
-                                Welcome
-                            </h1>
-                        </div>
+                    <div
+                        className="absolute text-2xl md:text-7xl text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                        <h1 className="text-4xl font-bold">
+                            Welcome
+                        </h1>
                     </div>
+                </div>
             </header>
         </>
     );
