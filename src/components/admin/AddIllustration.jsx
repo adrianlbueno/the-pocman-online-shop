2f18a30e47a37d3a55824a59a4e7cbaebeac3c0d
adrianbuenoo095 <adrianbueno095@gmail.com>
2025-01-29 23:47:21 +0100

    fix something in addIllustrion

--- src/components/admin/AddIllustration.jsx
+++ src/components/admin/AddIllustration.jsx
@@ -95,7 +95,7 @@ export const AddIllustration = () => {
                         <button onClick={()=> {
                             setForm(initialValues);
                             dispatch({
-                                type: "ADD_TASK",
+                                type: "ADD_ILLUSTRATION",
                                 payload: form
                             });
                         }} className="mt-5 bg-green-400 w-full hover:bg-green-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
