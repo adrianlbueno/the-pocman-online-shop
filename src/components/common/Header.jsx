3044b18fc68770303fb98db1b05b25bbce61761a
adrianbuenoo095 <adrianbueno095@gmail.com>
2025-02-16 19:19:37 +0100

    tide up code

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -6,17 +6,11 @@ const Header = () => {
         <>
             <header>
                 <div id="Home" className="flex items-center">
-                    <div className="relative w-max">
-                    <picture>
-                        <source srcSet={headerImage} media="(min-width: 1240px)"/>
-                        <img src={pocmanImage} alt="image of girl seated on bed"/>
-                    </picture>
-                    </div>
-                    <div
-                        className="absolute text-2xl md:text-7xl text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-                        <h1 className="text-4xl font-bold">
-                            Welcome
-                        </h1>
+                    <div className=" relative w-full">
+                        <picture>
+                            <source srcSet={headerImage} media="(min-width: 1240px)"/>
+                            <img className="bg-center bg-cover" src={pocmanImage} alt="image of girl seated on bed"/>
+                        </picture>
                     </div>
                 </div>
             </header>
