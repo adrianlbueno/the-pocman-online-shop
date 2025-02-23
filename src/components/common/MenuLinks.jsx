39a0bbf104298f938726f7770705e2dc9d7040d2
adrianbuenoo095 <adrianbueno095@gmail.com>
2025-02-23 07:34:43 +0100

    move context file to a different folder and a new link

--- src/components/common/MenuLinks.jsx
+++ src/components/common/MenuLinks.jsx
@@ -1,5 +1,6 @@
 export const menuLinks = [
     {id: "Home", label: 'Home', path: '/'},
+    {id: "Illustrations", label: 'Illustrations', path: '/work'},
     {id: "About", label: 'About Me', path: '/about'},
     {id: "Contact", label: 'Contact', path: '/contact'},
 ];
