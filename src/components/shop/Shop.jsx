6ee71b602a7da9b30ebcca0a3ed813a4aa7490bd
adrianbuenoo095 <adrianbueno095@gmail.com>
2025-02-23 07:48:12 +0100

    add a scroll ref

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -1,12 +1,19 @@
 import { Link } from "react-router-dom";
 import ShopItem from "./ShopItem";
 import {useFetchIllustrations} from "../../hooks/useFetchIllustrations.js";
+import {useRef} from "react";
 
 const Shop = () => {
     const illustrations = useFetchIllustrations()
+
+    const illustrationViewRef = useRef(null);
+    const handleScrollIllustrations = () => {
+        illustrationViewRef.current.scrollIntoView({ behavior: "smooth", block: "end" })
+
+    }
     return (
         <>
-            <div className="container px-5 py-24 mx-auto">
+            <div ref={illustrationViewRef} className="container px-5 py-24 mx-auto">
                 <div className="flex flex-col text-center w-full mb-20">
                     <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-fuchsia-600">The Pocman Illustration</h1>
                     <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Are you who you want to be?</p>
