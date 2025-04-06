1cbc15465cab63b0647178f23cdf82e058eff814
adrianbuenoo095 <adrianbueno095@gmail.com>
2025-04-06 22:27:17 +0200

    refactor code and fix some issues

--- src/components/admin/EditIllustration.jsx
+++ src/components/admin/EditIllustration.jsx
@@ -1,142 +1,130 @@
 import {useEffect, useState} from 'react';
-import {Link, useParams} from 'react-router-dom';
+import {useParams, useNavigate} from 'react-router-dom';
 import {useFetchIllustrations} from "../../hooks/useFetchIllustrations.js";
 
 export const EditIllustration = () => {
-
     const [illustrations] = useFetchIllustrations();
+    const [formData, setFormData] = useState(null);
 
-    const [selectedIllustration, setSelectedIllustration] = useState({
-        id: null,
-        title: "",
-        description: "",
-        price: 0,
-        image: "",
-    });
-    
     const {editId} = useParams();
-    const illustrationId = editId * 1;
-    const illustrationToUpDate = illustrations.find(
+    const navigate = useNavigate();
+    const illustrationId = Number(editId);
+
+    const illustrationToUpdate = illustrations.find(
         (illustration) => illustration.id === illustrationId
     );
 
     useEffect(() => {
-        setSelectedIllustration(illustrationToUpDate);
-    }, [illustrations, illustrationId]);
+        if (illustrationToUpdate) {
+            const {id, title, description, price, image} = illustrationToUpdate;
+            setFormData({id, title, description, price, image});
+        }
+    }, [illustrationToUpdate]);
 
-    const requestOptions = {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(selectedIllustration),
-    }
+    const handleOnSubmit = async () => {
+        const requestOptions = {
+            method: "PUT",
+            body: JSON.stringify(formData),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
 
-    const URL_API = "https://the-pocman-backend.onrender.com/illustrations";
-
-    const handleOnSubmit = (e) => {
-        e.preventDefault();
-        if (!selectedIllustration) return;
-        fetch(`${URL_API}${selectedIllustration.id}`, requestOptions)
-            .then(res => res.json())
-            .then(json => setSelectedIllustration(json.user))
+        const URL_API = `https://the-pocman-backend.onrender.com/illustrations/${formData.id}`;
+        await fetch(URL_API, requestOptions)
+            .then((res) => res.json()
+            )
+            .then(() => navigate("/admin"));
     };
 
-    const handleOnChange = (illustrationKey, newValue) =>
-        setSelectedIllustration({
-            ...selectedIllustration
-            , [illustrationKey]: newValue
+    const handleOnChange = (field) => (e) => {
+        const {value} = e.target;
+        setFormData({
+            ...formData,
+            [field]: value,
         });
+    };
 
-    if (!selectedIllustration || !selectedIllustration.id) {
-        return <div>Invalid Illustration ID.</div>;
+    if (!formData || !formData.id) {
+        return <div>Loading illustration...</div>;
     }
 
     return (
-        <>
-            <div className="w-full max-w-sm container mt-20 mx-auto">
-                <form onSubmit={handleOnSubmit}>
-                    <div className="w-full mb-5">
-                        <label
-                            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                            htmlFor="title"
-                        >
-                            Title
-                        </label>
-                        <input
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:text-gray-600 focus:shadow-outline"
-                            value={selectedIllustration
-                                .title}
-                            onChange={(e) => handleOnChange("title", e.target.value)}
-                            type="text"
-                            placeholder="Enter title"
-                        />
-                    </div>
-                    <div className="w-full  mb-5">
-                        <label
-                            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                            htmlFor="description"
-                        >
-                            Description
-                        </label>
-                        <input
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:text-gray-600 focus:shadow-outline"
-                            value={selectedIllustration
-                                .description}
-                            onChange={(e) => handleOnChange("description", e.target.value)}
-                            type="text"
-                            placeholder="Enter description"
-                        />
-                    </div>
-                    <div className="w-full  mb-5">
-                        <label
-                            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                            htmlFor="price"
-                        >
-                            Price
-                        </label>
-                        <input
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:text-gray-600 focus:shadow-outline"
-                            value={selectedIllustration
-                                .price}
-                            onChange={(e) => handleOnChange("price", parseFloat(e.target.value))}
-                            type="text"
-                            placeholder="Enter price"
-                        />
-                    </div>
-                    <div className="w-full  mb-5">
-                        <label
-                            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                            htmlFor="image"
-                        >
-                            Image URL
-                        </label>
-                        <input
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:text-gray-600 focus:shadow-outline"
-                            value={selectedIllustration
-                                .image}
-                            onChange={(e) => handleOnChange("image", e.target.value)}
-                            type="text"
-                            placeholder="Enter image URL"
-                        />
-                    </div>
-                    <div className="flex items-center justify-between">
+        <div className="w-full max-w-sm container mt-20 mx-auto">
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    handleOnSubmit(formData);
+                }}
+            >
+                <div className="w-full mb-5">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                        Title
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
+                        value={formData.title}
+                        onChange={handleOnChange("title")}
+                        type="text"
+                        placeholder="Enter title"
+                    />
+                </div>
 
-                        <Link to="/admin">
-                            <button onClick={handleOnSubmit}
-                                    className="block mt-5 bg-green-400 w-full hover:bg-green-500 text-white font-bold py-2 px-4 rounded focus:text-gray-600 focus:shadow-outline">
-                                Done
-                            </button>
-                        </Link>
-                        <Link to="/admin">
-                            <button
-                                className="block mt-5 bg-green-400 w-full hover:bg-green-500 text-white font-bold py-2 px-4 rounded focus:text-gray-600 focus:shadow-outline">
-                                Cancel
-                            </button>
-                        </Link>
-                    </div>
-                </form>
-            </div>
-        </>
+                <div className="w-full mb-5">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                        Description
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
+                        value={formData.description}
+                        onChange={handleOnChange("description")}
+                        type="text"
+                        placeholder="Enter description"
+                    />
+                </div>
+
+                <div className="w-full mb-5">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                        Price
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
+                        value={formData.price}
+                        onChange={handleOnChange("price")}
+                        type="text"
+                        placeholder="Enter price"
+                    />
+                </div>
+
+                <div className="w-full mb-5">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                        Image URL
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
+                        value={formData.image}
+                        onChange={handleOnChange("image")}
+                        type="text"
+                        placeholder="Enter image URL"
+                    />
+                </div>
+
+                <div className="flex items-center justify-between">
+                    <button
+                        type="submit"
+                        className="block mt-5 bg-green-400 w-full hover:bg-green-500 text-white font-bold py-2 px-4 rounded"
+                    >
+                        Done
+                    </button>
+                    <button
+                        type="button"
+                        onClick={() => navigate("/admin")}
+                        className="block mt-5 bg-gray-400 w-full hover:bg-gray-500 text-white font-bold py-2 px-4 rounded"
+                    >
+                        Cancel
+                    </button>
+                </div>
+            </form>
+        </div>
     );
 };
