a3fa07caa2a544184a85d0d4545e6fe51532f47b
adrianbuenoo095 <adrianbueno095@gmail.com>
2025-04-20 23:24:32 +0200

    update css

--- src/components/admin/IllustrationList.jsx
+++ src/components/admin/IllustrationList.jsx
@@ -13,21 +13,24 @@ const IllustrationList = () => {
                 </p>
             ) : (
                 illustrations.map(({id, title, description, price, image}) => (
-                    <div className="flex items-center bg-gray-100 mb-10 shadow " key={id}>
-                        <div className="flex-auto text-left px-4 py-2 m-2">
-                            <div className="w-full">
-                                <img src={image} alt={description} className="h-[100px] w-[100px]"/>
-                            </div>
-                            <p className="text-gray-900 leading-none">
+                    <div
+                        className="flex flex-col items-center  border rounded-lg shadow-sm md:flex-row md:max-w-xl "
+                        key={id}>
+
+                        <img src={image} alt={description}
+                             className="object-cover w-full h-96 md:h-auto md:w-48"/>
+                        <div className='flex flex-col justify-between p-4 leading-normal'>
+                            <p className=" font-bold text-gray-900 leading-none">
                                 {title}
                             </p>
-                            <p className="text-gray-600">
+                            <p className=" mb-2 font-normal text-gray-700">
                                 {description}
                             </p>
                             <span className="inline-block text-sm font-semibold mt-1">
                                 ${price}
                             </span>
                         </div>
+
                         <div className="flex-auto text-gray-800 text-right px-4 py-2 m-2">
                             <Link to={`/edit/${id}`} title="Edit Illustration">
                                 <FontAwesomeIcon icon={faEdit}/>
