c3c5604c858d5a16913eaccce1aa016bdff48909
adrianbuenoo095 <adrianbueno095@gmail.com>
2025-04-27 15:55:56 +0200

    update Sing in form

--- src/components/form/SignInForm.jsx
+++ src/components/form/SignInForm.jsx
@@ -1,39 +1,65 @@
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 const SignIn = () => {
     return (
-        <div className="flex items-center justify-center p-12">
-            <div className="w-full max-w-xs" >
-                <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" noValidate>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="inputEmail">E-mail</label>
-                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        <div className="flex min-h-screen  items-center justify-center p-12">
+            <div className="mx-auto w-full max-w-[550px]">
+                <h1 className="text-lg font-bold text-center">Welcome to the Pocman Store</h1>
+                <p className="text-center">Please enter your details</p>
+                <form noValidate>
+                    <div className="mb-5">
+                        <label
+                            htmlFor="inputEmail"
+                            className="mb-3 block text-base font-medium text-[#07074D]"
+                        >
+                            E-mail
+                        </label>
+                        <input
                             type="email"
                             id="inputEmail"
                             name="email"
+                            placeholder="Enter your email"
+                            className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                         />
                     </div>
-                    <div className="mb-6">
-                        <label className="lock text-gray-700 text-sm font-bold mb-2" htmlFor="inputPassword">Password</label>
+
+                    <div className="mb-5">
+                        <label
+                            htmlFor="inputPassword"
+                            className="mb-3 block text-base font-medium text-[#07074D]"
+                        >
+                            Password
+                        </label>
                         <input
-                            className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                             type="password"
                             id="inputPassword"
                             name="password"
+                            placeholder="Enter your password"
+                            className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                         />
                     </div>
-                    <div className="flex items-center justify-between">
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+
+                    <div className="flex items-center justify-between mb-5">
+                        <button
+                            type="button"
+                            className="hover:shadow-form rounded-md bg-blue-500 py-3 px-8 text-base font-semibold text-white outline-none"
+                        >
                             Sign In
                         </button>
-                        <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+                        <a
+                            href="#"
+                            className="text-sm font-semibold text-blue-500 hover:text-blue-700"
+                        >
                             Forgot Password?
                         </a>
                     </div>
                 </form>
-                <p className="mt-10 text-center text-sm text-gray-500">
-                    Not a member?
-                    <Link to="../signup" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Create an Account</Link>
+
+                <p className="mt-8 text-center text-sm text-gray-500">
+                    Not a member?{" "}
+                    <Link to="../signup" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                        Create an Account
+                    </Link>
                 </p>
             </div>
         </div>
