a156785c23f4486dc37f15b815416be423309fb4
adrianbuenoo095 <adrianbueno095@gmail.com>
2025-05-01 10:48:31 +0200

    update CartSidebar and ShopItem components

--- src/components/shop/ShopItem.jsx
+++ src/components/shop/ShopItem.jsx
@@ -4,8 +4,8 @@ const ShopItem = ({url, name}) => {
     const {addToCart} = useCartItems();
 
     return (
-        <div className="lg:w-1/3 sm:w-1/2 p-4">
-            <div className="relative flex w-96 flex-col  text-gray-700 shadow-md">
+        <div className="relative lg:w-1/3 sm:w-1/2 p-4">
+            <div className="flex w-96 flex-col  text-gray-700 shadow-md">
                 <div
                     className="bg-gradient-to-r from-[#f02aa6] to-[#ff6f48] hover:bg-gradient-to-r hover:from-[#fff] hover:to-[#ff6f48]">
                     <img src={url} alt={name} className="h-full w-full object-cover hover:opacity-50 "/>
