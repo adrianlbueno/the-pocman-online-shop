c70320985565e0e5ea8fc5651e55474786ab7e98
adrianbuenoo095 <adrianbueno095@gmail.com>
2025-05-01 11:02:03 +0200

    update CartSidebar

--- src/components/cart/CartSidebar.jsx
+++ src/components/cart/CartSidebar.jsx
@@ -6,9 +6,8 @@ const CartSidebar = ({open, toggleCart}) => {
     if (!open) return null;
     return (
         <div className="fixed inset-0 z-50" aria-labelledby="slide-over-title" aria-modal="true">
-
             <div
-                className="absolute inset-0 bg-black bg-opacity-50"
+                className="absolute inset-0"
                 onClick={toggleCart}
             ></div>
 
