c9e0d0db1f6457eb38d581f39c54307e07585844
adrianbuenoo095 <adrianbueno095@gmail.com>
2025-05-02 23:00:12 +0200

    update coming page

--- src/components/common/ComingSoon.jsx
+++ src/components/common/ComingSoon.jsx
@@ -2,38 +2,30 @@ import React from "react";
 import {useFetchIllustrations} from "../../hooks/useFetchIllustrations";
 
 export default function ComingSoon() {
-    const [illustrations, isLoading] = useFetchIllustrations();
+    /*
+        const [illustrations, isLoading] = useFetchIllustrations();
 
-    const getDailyIllustration = () => {
-        if (illustrations.length === 0) return null;
+        const getDailyIllustration = () => {
+            if (illustrations.length === 0) return null;
 
-        const today = new Date();
-        const dayOfYear = Math.floor(
-            (today - new Date(today.getFullYear(), 0, 0)) / (1000 * 60 * 60 * 24)
-        );
+            const today = new Date();
+            const dayOfYear = Math.floor(
+                (today - new Date(today.getFullYear(), 0, 0)) / (1000 * 60 * 60 * 24)
+            );
 
-        const index = dayOfYear % illustrations.length;
-        return illustrations[index];
-    };
+            const index = dayOfYear % illustrations.length;
+            return illustrations[index];
+        };
 
-    const dailyIllustration = getDailyIllustration();
+        const dailyIllustration = getDailyIllustration();
+
+     */
 
     return (
 
         <div
             className="flex flex-col justify-center items-center min-h-screen p-5 text-center text-[32px] font-sans bg-[#F406E6]">
-            {isLoading && <p>Loading...</p>}
-
-            {dailyIllustration && (
-                <div className="aspect-w-16 aspect-h-9">
-                    <h2 className="text-white">Please check back soon.</h2>
-                    <img
-                        className=" w-96 object-cover"
-                        src={dailyIllustration.image}
-                        alt="Daily Pocman Drop"
-                    />
-                </div>
-            )}
+            <h2 className="text-white">Please check back soon.</h2>
         </div>
     );
 }
