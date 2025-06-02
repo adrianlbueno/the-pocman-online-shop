3777ede489d360ed780bc7e519d2c22d5df55ce1
adrianbuenoo095 <adrianbueno095@gmail.com>
2025-06-02 23:42:01 +0200

    update the footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,6 +1,36 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBehance, faGithub, faInstagram, faLinkedin} from "@fortawesome/free-brands-svg-icons";
+import {Link} from "react-router-dom";
+
 const getYear = () => {
     return new Date().getFullYear();
 }
+const instagramLink = (
+    <Link to="https://www.instagram.com/thepocman/" target="_blank">
+        <FontAwesomeIcon className="w-[25px]" icon={faInstagram}/>
+    </Link>
+);
+
+const linkedinLink = (
+    <Link
+        to="https://www.linkedin.com/in/adrian-bueno-08189b108/"
+        target="_blank"
+    >
+        <FontAwesomeIcon className="w-[25px]" icon={faLinkedin}/>
+    </Link>
+);
+
+const behanceLink = (
+    <Link to="https://www.behance.net/thepocman" target="_blank">
+        <FontAwesomeIcon className="w-[25px]" icon={faBehance}/>
+    </Link>
+);
+
+const githubLink = (
+    <Link to="https://github.com/adrianlbueno" target="_blank">
+        <FontAwesomeIcon className="w-[25px]" icon={faGithub}/>
+    </Link>
+);
 
 const Footer = () => {
     return (
@@ -9,6 +39,12 @@ const Footer = () => {
                 <div className="w-full max-w-full sm:w-3/4 mx-auto text-center">
                     <span
                         className="text-sm text-gray-600 py-1"> &copy;{getYear()} All Rights Reversed by Adrian Bueno</span>
+                    <div className="flex gap-3 justify-center py-1">
+                        <span>{instagramLink}</span>
+                        <span>{linkedinLink}</span>
+                        <span>{githubLink}</span>
+                        <span>{behanceLink}</span>
+                    </div>
                 </div>
             </footer>
         </>
