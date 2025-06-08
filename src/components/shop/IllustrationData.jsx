9efda5013e3a5634e06281ee3adbbf3b538057d8
adrianbuenoo095 <adrianbueno095@gmail.com>
2025-06-08 11:02:49 +0200

    bug fixes, update vit.config, upgrade react dom

--- src/components/shop/IllustrationData.jsx
+++ src/components/shop/IllustrationData.jsx
@@ -2,10 +2,11 @@ import ShopItem from "./ShopItem";
 import {Link} from "react-router-dom";
 
 const IllustrationData = ({data}) => {
+
     return (
         <>
             {data && data.map((illustration) => (
-                <div key={illustration.id}>
+                <div key={illustration._id}>
                     <Link to={`/illustrations/${illustration.id}`}>
                         <ShopItem id={illustration.id} url={illustration.image} name={illustration.name}/>
                     </Link>
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,23 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {sentryVitePlugin} from "@sentry/vite-plugin";
+import {vitePlugin as remix} from "@remix-run";
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "the-pocman",
-    project: "javascript-react"
-  })],
+    plugins: [react(),
+        remix({
+            future: {
+                v7_skipActionErrorRevalidation: true,
+            },
+        }),
+        sentryVitePlugin({
+                org: "the-pocman",
+                project: "javascript-react"
+            },
+        )],
 
-  build: {
-    sourcemap: true
-  }
-})
+    build: {
+        sourcemap: true
+    }
+});
