8db2a3230528cde85882fda090ee8c7e205d9249
adrianbuenoo095 <adrianbueno095@gmail.com>
2025-08-11 21:51:29 +0200

    update the comingpage

--- src/components/common/ComingSoon.jsx
+++ src/components/common/ComingSoon.jsx
@@ -1,40 +1,37 @@
 import React from "react";
-import {useFetchIllustrations} from "../../hooks/useFetchIllustrations";
+import { useFetchIllustrations } from "../../hooks/useFetchIllustrations";
 
 export default function ComingSoon() {
+  const [illustrations, isLoading] = useFetchIllustrations();
 
-    const [illustrations, isLoading] = useFetchIllustrations();
+  const getDailyIllustration = () => {
+    if (illustrations.length === 0) return null;
 
-    const getDailyIllustration = () => {
-        if (illustrations.length === 0) return null;
-
-        const today = new Date();
-        const dayOfYear = Math.floor(
-            (today - new Date(today.getFullYear(), 0, 0)) / (1000 * 60 * 60 * 24)
-        );
-
-        const index = dayOfYear % illustrations.length;
-        return illustrations[index];
-    };
-
-    const dailyIllustration = getDailyIllustration();
-
-
-    return (
-        <div
-            className="flex flex-col justify-center items-center min-h-screen p-5 text-center text-[32px] font-sans bg-[#F406E6] text-white">
-            {isLoading && <p>Loading...</p>}
-
-            {dailyIllustration && (
-                <div className="aspect-w-16 aspect-h-9">
-                    <h2 className="text-white">Please check back soon.</h2>
-                    <img
-                        className=" w-96 object-cover"
-                        src={dailyIllustration.image}
-                        alt="Daily Pocman Drop"
-                    />
-                </div>
-            )}
-        </div>
+    const today = new Date();
+    const dayOfYear = Math.floor(
+      (today - new Date(today.getFullYear(), 0, 0)) / (1000 * 60 * 60 * 24)
     );
+
+    const index = dayOfYear % illustrations.length;
+    return illustrations[index];
+  };
+
+  const dailyIllustration = getDailyIllustration();
+
+  return (
+    <div className="font-nunito flex flex-col justify-center items-center min-h-screen p-5 text-center text-[32px] font-sans bg-[#F406E6] text-white">
+      {isLoading && <p>Loading...</p>}
+
+      {dailyIllustration && (
+        <div className="aspect-w-16 aspect-h-9">
+          <h2 className="text-white">Please check back soon.</h2>
+          <img
+            className=" w-96 object-cover"
+            src={dailyIllustration.image}
+            alt="Daily Pocman Drop"
+          />
+        </div>
+      )}
+    </div>
+  );
 }
