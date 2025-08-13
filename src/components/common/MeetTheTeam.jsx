155adc2dd10df245e0a7034c1a224fad5ead2337
adrianbuenoo095 <adrianbueno095@gmail.com>
2025-08-13 21:26:19 +0200

    update meettheteam component

--- src/components/common/MeetTheTeam.jsx
+++ src/components/common/MeetTheTeam.jsx
@@ -13,6 +13,37 @@ const MeetTheTeam = () => {
           <p className="font-nunito text-[50px] text-gray-600 mb-8">
             We are creators, photographers and entrepreneurs.
           </p>
+          <div className="flex flex-wrap justify-center mt-8">
+            <div className="text-center px-4 mb-6">
+              <img
+                className="w-32 h-32 object-cover rounded-full mx-auto"
+                src={secondUserImage}
+                alt="image of Adrian"
+              />
+              <div className=" font-nunito text-xl mt-2">Adrian</div>
+              <div className=" font-nunito text-xl mt-2">
+                Founder & Creative Director
+              </div>
+            </div>
+            <div className="text-center px-4 mb-6">
+              <img
+                className="w-32 h-32 object-cover rounded-full mx-auto"
+                src={firstuserImage}
+                alt="image of Ribert"
+              />
+              <p className=" font-nunito text-xl mt-2">Ribert</p>
+              <div className=" font-nunito text-xl mt-2">Co-founder</div>
+            </div>
+            <div className="text-center px-4 mb-6">
+              <img
+                className="w-32 h-32 object-cover rounded-full mx-auto"
+                src={thirdUserImage}
+                alt="image of Jonathan"
+              />
+              <div className="font-nunito text-xl mt-2">Jonathan</div>
+              <div className="font-nunito text-xl mt-2">Co-founder</div>
+            </div>
+          </div>
         </div>
 
         <hr className="border-separate"></hr>
@@ -26,38 +57,6 @@ const MeetTheTeam = () => {
           </p>
         </div>
         <hr className="border-separate"></hr>
-
-        <div className="flex flex-wrap justify-center mt-8">
-          <div className="text-center px-4 mb-6">
-            <img
-              className="w-32 h-32 object-cover rounded-full mx-auto"
-              src={secondUserImage}
-              alt="image of Adrian"
-            />
-            <div className=" font-nunito text-xl mt-2">Adrian</div>
-            <div className=" font-nunito text-xl mt-2">
-              Founder & Creative Director
-            </div>
-          </div>
-          <div className="text-center px-4 mb-6">
-            <img
-              className="w-32 h-32 object-cover rounded-full mx-auto"
-              src={firstuserImage}
-              alt="image of Ribert"
-            />
-            <p className=" font-nunito text-xl mt-2">Ribert</p>
-            <div className=" font-nunito text-xl mt-2">Co-founder</div>
-          </div>
-          <div className="text-center px-4 mb-6">
-            <img
-              className="w-32 h-32 object-cover rounded-full mx-auto"
-              src={thirdUserImage}
-              alt="image of Jonathan"
-            />
-            <div className="font-nunito text-xl mt-2">Jonathan</div>
-            <div className="font-nunito text-xl mt-2">Co-founder</div>
-          </div>
-        </div>
       </div>
     </div>
   );
