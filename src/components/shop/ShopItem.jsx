d87c7a44c52566539209a880c645a2188d3c6210
adrianbuenoo095 <adrianbueno095@gmail.com>
2025-08-20 22:43:40 +0200

    update shopitem component

--- src/components/shop/ShopItem.jsx
+++ src/components/shop/ShopItem.jsx
@@ -2,14 +2,27 @@ import { useCartItems } from "../../context/cart/CartContext.jsx";
 
 const ShopItem = ({ url, name }) => {
   const { addToCart } = useCartItems();
-  console.log("ShopItem rendered", url, name);
   return (
-    <div className="relative group lg:w-1/3 sm:w-1/2 p-4">
-      <div className="flex w-96 flex-col  text-gray-700 shadow-md">
-        <div className=" relative group">
-          <img src={url} alt={name} className="h-full w-full object-cover  " />
-          <div className="opacity-0 group-hover:opacity-50 duration-300 absolute inset-0 flex items-center justify-center bg-gradient-to-r from-[#f02aa6] to-[#ff6f48]  text-xl text-black font-nunito">
-            {name}
+    <div className="group relative lg:w-1/3 sm:w-1/2 p-4">
+      <div className="flex w-96 flex-col text-gray-700 shadow-md">
+        <div className="group relative">
+          <img src={url} alt={name} className="w-full h-full object-cover" />
+          <div
+            className="
+                absolute inset-0
+                bg-black/60
+                opacity-0
+                group-hover:opacity-100
+                transition-opacity
+                duration-300
+                backdrop-filter
+                backdrop-saturate-100  
+                group-hover:backdrop-saturate-200  
+              "
+          >
+            <div className="absolute inset-0 p-6 flex flex-col justify-end">
+              <span className="text-sm text-cyan-400">{name}</span>
+            </div>
           </div>
         </div>
       </div>
