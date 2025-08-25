9cb2d94dd0c39e116e5eeac872dcdcca4c3d0e07
adrianbuenoo095 <adrianbueno095@gmail.com>
2025-08-25 20:27:13 +0200

    change font size

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -7,19 +7,18 @@ const NotFound = () => {
     <>
       <div className="w-full px-16 md:px-0 h-screen flex items-center justify-center">
         <div className="flex flex-col items-center justify-center px-4 md:px-8 lg:px-24 py-8">
-          <p className=" font-nunito font-bold text-6xl md:text-7xl lg:text-9xl  tracking-wider text-[#C025D3]">
+          <p className=" font-nunito font-light text-6xl md:text-7xl lg:text-7xl  tracking-wider text-[#C025D3]">
             404
           </p>
           <p className="text-2xl md:text-3xl lg:text-5xl font-nunito font-extrabold tracking-wider mt-4">
-            Page Not Found
+            Oops, Page Not Found
           </p>
 
           <Link to="/">
             <button
               type="button"
-              className=" font-nunito flex items-center space-x-2 bg-[#C025D3] hover:bg-blue-700 text-gray-100 px-4 py-2 mt-6 rounded transition duration-150"
+              className=" font-nunito flex items-center space-x-2 bg-[#C025D3] hover: font-extrabold text-white px-4 py-2 mt-6 rounded transition duration-150"
             >
-              <FontAwesomeIcon className="mr-2.5" icon={faArrowLeft} />
               Return Home
             </button>
           </Link>
