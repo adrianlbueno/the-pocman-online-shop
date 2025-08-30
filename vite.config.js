b1f23da77b56d88e16c309f831d4d4ff0fd7bbab
adrianbuenoo095 <adrianbueno095@gmail.com>
2025-08-30 13:02:17 +0200

    testing

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,23 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 
 export default defineConfig({
   plugins: [
     react(),
     sentryVitePlugin({
-      org: "the-pocman",
-      project: "javascript-react",
+      org: 'the-pocman',
+      project: 'javascript-react',
     }),
   ],
+  css: {
+    modules: {
+      localsConvention: 'camelCase',
+      generateScopedName: '[local]_[hash:base64:2]',
+    },
+  },
   build: {
-    outDir: "dist",
+    outDir: 'dist',
     emptyOutDir: true,
     sourcemap: true,
   },
