7293abd07264987109cb4a0e6b404e030594dd82
adrianbuenoo095 <adrianbueno095@gmail.com>
2025-09-17 21:01:08 +0200

    fix issue with protected route

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,9 +1,17 @@
 import { Navigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from '../context/Auth/AuthContext.jsx';
 
-const ProtectedRoute = ({ isLogin }) => {
-  if (!isLogin) {
-    return <Navigate to="/" replace />;
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated } = useContext(AuthContext);
+
+  console.log('isAuthenticated', isAuthenticated);
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
   }
+
+  return children;
 };
 
 export default ProtectedRoute;
