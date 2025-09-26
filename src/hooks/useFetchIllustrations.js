6507543edcf53ccdf8cfea565393060850818fc5
adrianbuenoo095 <adrianbueno095@gmail.com>
2025-09-26 19:40:30 +0200

    update the useFetchIllustrations hook

--- src/hooks/useFetchIllustrations.js
+++ src/hooks/useFetchIllustrations.js
@@ -1,11 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 
 export const useFetchIllustrations = () => {
   const URL_API = `${process.env.VITE_APP_ONRENDER_URI}/illustrations`;
   const [illustrations, setIllustrations] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const fetchData = async () => {
+  const fetchData = useCallback(async () => {
     try {
       setIsLoading(true);
       const response = await fetch(URL_API);
@@ -17,10 +17,10 @@ export const useFetchIllustrations = () => {
     } catch (error) {
       console.error('Error fetching illustrations:', error);
     }
-  };
+  }, []);
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [fetchData]);
   return [illustrations, isLoading];
 };
