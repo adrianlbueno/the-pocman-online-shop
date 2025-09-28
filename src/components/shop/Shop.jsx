6a471d44536b0345cd6b629b5063f5f05ab5620d
adrianbuenoo095 <adrianbueno095@gmail.com>
2025-09-28 14:21:59 +0200

    update styling

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -8,9 +8,9 @@ const Shop = () => {
   return (
     <>
       <div className="container mx-auto px-5 overflow-hidden">
-        <div className="flex justify-center">
+        <div className="flex justify-center text-center">
+          {isLoading && <Loading />}
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-            {isLoading && <Loading />}
             <IllustrationData data={illustrations} />
           </div>
         </div>
