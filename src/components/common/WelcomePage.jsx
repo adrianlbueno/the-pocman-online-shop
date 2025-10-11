6f8d5ea19649fbb6d0d51a060d1a74495236f1c7
adrianbuenoo095 <adrianbueno095@gmail.com>
2025-10-11 11:23:16 +0200

    :)

--- src/components/common/WelcomePage.jsx
+++ src/components/common/WelcomePage.jsx
@@ -1,4 +1,4 @@
-const WelcomePage = (isLoading) => {
+const WelcomePage = ({ isLoading }) => {
   return (
     <div
       className={`flex font-nunito font-extrabold text-[64px] text-white  bg-[#F406E6] justify-center text-center items-center transition-all duration-500 ease-in-out ${
