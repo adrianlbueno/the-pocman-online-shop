1da06093fd0ebcc453976fcc9357a153e052479c
adrianbuenoo095 <adrianbueno095@gmail.com>
2025-11-10 20:54:47 +0100

    reduce font size on WelcomePage

--- src/components/common/WelcomePage.jsx
+++ src/components/common/WelcomePage.jsx
@@ -2,7 +2,7 @@ const WelcomePage = ({ isLoading }) => {
   return (
     <>
       <div
-        className={`flex font-nunito font-extrabold text-[64px] text-white bg-[#F406E6] justify-center text-center items-center transition-all duration-500 ease-in-out ${
+        className={`flex font-nunito font-extrabold text-[32px] text-white bg-[#F406E6] justify-center text-center items-center transition-all duration-500 ease-in-out ${
           isLoading ? 'h-[100vh]' : 'max-h-0'
         } `}
       >
