6d5a1709f1e5b9a451e1927dab8b86d1f8afead9
adrianbuenoo095 <adrianbueno095@gmail.com>
2025-12-03 18:49:14 +0100

    improve layout in ItemDetailsPage component

--- src/pages/ItemDetailsPage.jsx
+++ src/pages/ItemDetailsPage.jsx
@@ -8,10 +8,10 @@ const ItemDetailsPage = () => {
   const { _illustrationId } = useParams();
   const navigate = useNavigate();
 
-  if (illustrations.length === 0) return;
+  if (isLoading || !illustrations || illustrations.length === 0) return;
 
   const currentIndex = illustrations.findIndex(
-    (item) => item._id === _illustrationId
+    (illustration) => illustration._id === _illustrationId
   );
 
   const illustration = illustrations[currentIndex];
@@ -22,43 +22,45 @@ const ItemDetailsPage = () => {
 
   return (
     <div className="container mx-auto px-5 pt-6">
-      <div className="flex flex-wrap lg:w-2/3 mx-auto">
-        <img
-          alt={illustration.title}
-          className="max-h-[60vh]
-              w-auto
-              lg:max-w-[60vh]
-              object-contain
-              rounded"
-          src={illustration.image}
-        />
-        <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-          <h2 className="text-lg title-font text-gray-500 tracking-widest font-nunito font-bold">
-            {illustration.title}
-          </h2>
-          <p className="leading-relaxed font-nunito">
-            {illustration.description}
-          </p>
+      <div className="lg:w-2/3 mx-auto">
+        <div className="flex flex-col lg:flex-row">
+          <div className="w-full lg:w-1/2">
+            <div className="h-[60vh] flex items-center justify-center">
+              <img
+                alt={illustration.title}
+                className="max-h-full w-auto object-contain rounded"
+                src={illustration.image}
+              />
+            </div>
+          </div>
+
+          <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+            <h2 className="text-lg title-font text-gray-500 tracking-widest font-nunito font-bold">
+              {illustration.title}
+            </h2>
+            <p className="leading-relaxed font-nunito">
+              {illustration.description}
+            </p>
+          </div>
         </div>
       </div>
+
       <div className="flex items-center justify-center mt-4 lg:w-4/5 mx-auto font-nunito">
         <button
-          className=" px-4 py-2 text-black"
+          className="px-4 py-2 text-black"
           onClick={() =>
             navigate(`/illustrations/${illustrations[prevIndex]._id}`)
           }
         >
-          {/*<FontAwesomeIcon icon={faAngleLeft} />*/}
           Prev
         </button>
         <span>/</span>
         <button
-          className="px-4 py-2  text-black"
+          className="px-4 py-2 text-black"
           onClick={() =>
             navigate(`/illustrations/${illustrations[nextIndex]._id}`)
           }
         >
-          {/*<FontAwesomeIcon icon={faAngleRight} />*/}
           Next
         </button>
       </div>
